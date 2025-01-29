dcd80a6fc9d6259ec6dd798afd0f3cef2da5187b
Tanish Dwivedi <tanishdwivedi847@gmail.com>
2025-01-29 10:26:02 +0530

    made changes to package.json

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,4 +3,4 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api",
   withCredentials: true,
-});
+}); 
