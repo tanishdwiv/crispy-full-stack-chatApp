5b9ea002d9742e758ea50c2e5a8bca373148f840
Tanish Dwivedi <tanishdwivedi847@gmail.com>
2025-01-29 07:16:53 +0530

    package.json added

--- Frontend/src/store/useChatStore.js
+++ Frontend/src/store/useChatStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
-import { useAuthStore } from "./useAuthStore";
+import { useAuthStore } from "../useAuthStore";
 
 export const useChatStore = create((set, get) => ({
   messages: [],
